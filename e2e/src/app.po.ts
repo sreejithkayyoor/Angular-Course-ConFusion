8003daa13620d67050f92821cd44c72e6976a124
Sreejith S <sreejithkayyoor@gmail.com>
2019-04-14 22:57:12 +0530

    Course completed

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,20 @@
 import { browser, by, element } from 'protractor';
+import { StringMap } from '@angular/core/src/render3/jit/compiler_facade_interface';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(link: string) {
+    return browser.get(link);
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
+  }
+
+  getElement(selector: string){
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string){
+    return element.all(by.css(selector));
   }
 }
